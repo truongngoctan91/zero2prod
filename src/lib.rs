3fd5ca6cb6e10d0945bbff4329e7c2db40c868eb
truongngoctan91 <truongngoctanbn@gmail.com>
2024-07-27 21:11:39 +0200

    add reject 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 pub mod configuration;
+pub mod domain;
+// new entry
+pub mod email_client;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
+
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
